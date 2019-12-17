d32e24270077cef6b0f106a862a2a8e2a6bd8542
hbarsaiyan <hbarsaiyan@gmail.com>
2019-12-17 11:54:27 +0000

    Update 3

--- GravityGuy_Main.py
+++ GravityGuy_Main.py
@@ -1,6 +1,7 @@
 import pygame as pg
 import obstacle as ob
 
+pg.init()
 # Program Constants
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -14,37 +15,73 @@ bg = pg.image.load("bg.png")
 bg_block = pg.image.load("bgblock1.png")
 display_width, display_height = 800, 600
 block_thickness = 40
-char_VEL = 15
+char_VEL = 21
 
 restart = True
-FPS = 30 #if u want to change this u have to change values in Player.render too
+FPS = 30  # if u want to change this u have to change values in Player.render too
 i = 0
 
 
-class Player:
+class Player:  # Rambo
     def __init__(self):
-        self.y_co = display_height/3 + 250 - block_thickness
+        self.x_co = 162
+        self.y_co = display_height / 3 + 250 - block_thickness
         self.vel = 0
         self.i = 0
         self.j = 0
+        self.hitbox = (self.x_co + 16, self.y_co, 22, 37)
 
     def change_gravity(self):
         if self.y_co == display_height / 3:
             self.vel = char_VEL
             self.i = 0
-        elif self.y_co == display_height/3 + 250 - block_thickness:
+        elif self.y_co == display_height / 3 + 250 - block_thickness:
             self.vel = -char_VEL
             self.i = 1
 
     def render(self):
-        ROOT.blit(char[self.i][self.j], (100, self.y_co))
+        ROOT.blit(char[self.i][self.j], (self.x_co, self.y_co))
         if char_walk_index in range(0, 5) or char_walk_index in range(10, 15) or char_walk_index in range(20, 25):
             self.j = 1
         else:
             self.j = 0
+        self.hitbox = (self.x_co + 16, self.y_co + 3, 22, 37)
 
     def move(self):
         self.y_co += self.vel
+
+    '''def get_mask(self):
+        """
+        gets the mask for the current image of the player
+        """
+        return pg.mask.from_surface(char[self.i][self.j])'''
+
+
+def collide(p1, rand, obstacle_x,OVER):
+    if obstacle_x == 180 or obstacle_x == 220:
+        if rand[0] == 1:  # 1
+            if rand[1] == 1:  # UP
+                ob_y_co = 200
+                if ob_y_co <= p1.hitbox[1] <= ob_y_co + 10:
+                    OVER = False
+                    print("HIT1")
+            else:  # DOWN
+                ob_y_co = 450
+                if ob_y_co - 10 <= p1.y_co + 40 <= ob_y_co:
+                    OVER = False
+                    print("HIT2")
+    if obstacle_x == 200:
+        if rand[0] == 1:  # 1
+            if rand[1] == 1:  # UP
+                ob_y_co = 200
+                if ob_y_co <= p1.hitbox[1] <= ob_y_co + 40:
+                    OVER = False
+                    print("HIT3")
+            else:  # DOWN
+                ob_y_co = 450
+                if ob_y_co - 40 <= p1.y_co + 40  <= ob_y_co:
+                    OVER = False
+                    print("HIT4")
 
 
 ROOT = pg.display.set_mode((display_width, display_height))
@@ -52,14 +89,20 @@ ROOT = pg.display.set_mode((display_width, display_height))
 
 def main_loop():
     # Game Loop Variables
-    global restart, char_walk_index
+    global restart, char_walk_index,OVER
     p1 = Player()
     clock = pg.time.Clock()
-    BG_VEL = 4
-    obstacle_x = 800
-    obstacle_x1 = 780
+    BG_VEL = 10
     bg_X, char_walk_index = 0, 0
-
+    # obstacle Part
+    obstacle_x = 800
+    obstacle_x1 = 1000
+    obstacle_x2 = 1200
+    obstacle_x3 = 1400
+    rand1 = ob.random()
+    rand2 = ob.random()
+    rand3 = ob.random()
+    rand4 = ob.random()
     # Game Loop
     OVER = False
     while not OVER:
@@ -74,26 +117,48 @@ def main_loop():
 
         # Logic Section
         p1.move()
-        if p1.y_co == display_height / 3 or p1.y_co == display_height/3 + 250 - block_thickness:
+        if p1.y_co == display_height / 3 or p1.y_co == display_height / 3 + 250 - block_thickness:
             p1.vel = 0
 
         # Rendering Section
         ROOT.fill((13, 13, 13))
-        pg.draw.rect(ROOT, BLACK, (0, 0, display_width, display_height/3))
-        pg.draw.rect(ROOT, BLACK, (0, display_height/3 + 250, display_width, display_height/3))
+        pg.draw.rect(ROOT, BLACK, (0, 0, display_width, display_height / 3))
+        pg.draw.rect(ROOT, BLACK, (0, display_height / 3 + 250, display_width, display_height / 3))
 
         if bg_X >= -20:  # Background Render + animation
-            ROOT.blit(bg_block, (bg_X, display_height/3 - 20))
+            ROOT.blit(bg_block, (bg_X, display_height / 3 - 20))
             bg_X -= BG_VEL
-            #ob.simple_spike()
+
         else:
-            ROOT.blit(bg_block, (bg_X, display_height/3 - 20))
+            ROOT.blit(bg_block, (bg_X, display_height / 3 - 20))
             bg_X = 0
 
         ROOT.blit(bg, (0, display_height / 3 - 20))
         p1.render()  # Player Render
-        obstacle_x = ob.big_spike(ROOT, obstacle_x )
+        obstacle_x = ob.rand_obstacle(ROOT, obstacle_x, rand1)
+        obstacle_x1 = ob.rand_obstacle(ROOT, obstacle_x1, rand2)
+        obstacle_x2 = ob.rand_obstacle(ROOT, obstacle_x2, rand3)
+        obstacle_x3 = ob.rand_obstacle(ROOT, obstacle_x3, rand4)
         obstacle_x -= BG_VEL
+        obstacle_x1 -= BG_VEL
+        obstacle_x2 -= BG_VEL
+        obstacle_x3 -= BG_VEL
+        collide(p1, rand1, obstacle_x,OVER)
+        collide(p1, rand2, obstacle_x1,OVER)
+        collide(p1, rand3, obstacle_x2,OVER)
+        collide(p1, rand4, obstacle_x3,OVER)
+        if obstacle_x < 0:
+            obstacle_x = 800
+            rand1 = ob.random()
+        if obstacle_x1 < 0:
+            obstacle_x1 = 800
+            rand2 = ob.random()
+        if obstacle_x2 < 0:
+            obstacle_x2 = 800
+            rand3 = ob.random()
+        if obstacle_x3 < 0:
+            obstacle_x3 = 800
+            rand4 = ob.random()
 
         clock.tick_busy_loop(FPS)
         pg.display.update()
--- obstacle.py
+++ obstacle.py
@@ -1,7 +1,28 @@
 import pygame as pg
+import random as rand
 
 spike = pg.image.load("spike.png")
 block = pg.image.load("block.png")
+
+
+def random():
+    obstacle_no = rand.randrange(1, 3)
+    obstacle_side = rand.randrange(0, 2)
+    return obstacle_no, obstacle_side
+
+
+def rand_obstacle(ROOT, obstacle_x, rand):
+    if rand[1] == 0:
+        side = "DOWN"
+    else:
+        side = "UP"
+
+    if rand[0] == 1:
+        simple_spike(ROOT, obstacle_x, side=side)
+        return obstacle_x
+    elif rand[0] == 2:
+        big_spike(ROOT, obstacle_x, side=side)
+        return obstacle_x
 
 
 def blitRotateCenter(surf, image, topleft, angle):
@@ -11,30 +32,40 @@ def blitRotateCenter(surf, image, topleft, angle):
     surf.blit(rotated_image, new_rect.topleft)
 
 
-def simple_spike(ROOT, obstacle_x, side="DOWN"):
+def simple_spike(ROOT, obstacle_x, side="DOWN"):  # 1
     if side == "UP":
         obstacle_y = 200
         tilt = 180
+        blitRotateCenter(ROOT, spike, (obstacle_x, obstacle_y + 20), tilt)
     else:
         obstacle_y = 430
         tilt = 0
-    blitRotateCenter(ROOT, spike, (obstacle_x, obstacle_y), tilt)
+        blitRotateCenter(ROOT, spike, (obstacle_x, obstacle_y - 20), tilt)
+    blitRotateCenter(ROOT, spike, (obstacle_x - 20, obstacle_y), 90)
+    ROOT.blit(block, (obstacle_x, obstacle_y))
+    blitRotateCenter(ROOT, spike, (obstacle_x + 20, obstacle_y), 270)
+
+    #spikes.append((obstacle_x, obstacle_y))
     return obstacle_x
 
 
-def big_spike(ROOT, obstacle_x, side="DOWN"):
+def big_spike(ROOT, obstacle_x, side="DOWN"):  # 2
     if side == "UP":
         obstacle_y = 200
         tilt = 180
-        for i in range(1, 4):
+        for i in range(1, 5):  # block
+            blitRotateCenter(ROOT, spike, (obstacle_x - 20, obstacle_y), 90)
             ROOT.blit(block, (obstacle_x, obstacle_y))
+            blitRotateCenter(ROOT, spike, (obstacle_x + 20, obstacle_y), 270)
             obstacle_y += 20
-        blitRotateCenter(ROOT, spike, (obstacle_x, obstacle_y), tilt)
+        blitRotateCenter(ROOT, spike, (obstacle_x, obstacle_y), tilt)  # spike
     else:
         obstacle_y = 430
         tilt = 0
-        for i in range(1, 4):
+        for i in range(1, 5):  # block
+            blitRotateCenter(ROOT, spike, (obstacle_x - 20, obstacle_y), 90)
             ROOT.blit(block, (obstacle_x, obstacle_y))
+            blitRotateCenter(ROOT, spike, (obstacle_x + 20, obstacle_y), 270)
             obstacle_y -= 20
-        blitRotateCenter(ROOT, spike, (obstacle_x, obstacle_y), tilt)
+        blitRotateCenter(ROOT, spike, (obstacle_x, obstacle_y), tilt)  # spike
     return obstacle_x
